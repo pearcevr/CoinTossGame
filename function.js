a5b4221734cd3432fa8287462ab542e140cae40a
pearcevr <pearcevr@clarkson.edu>
2023-12-01 22:50:31 -0500

    changes

--- function.js
+++ function.js
@@ -1,27 +1,42 @@
 const choices= ["heads", "tails"];
+var image;
+
+function wait(){
+    image = "wait.png";
+    document.getElementById('changeCat').src = image;
+}
 
 function outcome(){
-    return choices[Math.floor(Math.random*2)];
+    return choices[Math.floor(Math.random()*2)];
 }
 
 function play(playerChoice,compResult){
     if(playerChoice==compResult){
-        return "You win!";
+        return "win! :D";
     }else{
-        return "You lose!"
+        return "lose! :C"
     }
 }
 
 const cats = document.querySelectorAll("button");
 const choiceText = document.querySelector(".choiceText");
 const resultText = document.querySelector(".resultText");
+const winLoseText = document.querySelector(".winLose")
 
 cats.forEach((button)=>{
     button.addEventListener("click", ()=>{
         const playerChoice = button.id;
         const compResult = outcome();
-        play(playerChoice,compResult);
-        choiceText.textContent = 'You choose: ${playerChoice}!';
-        resultText.textContent = 'You get: ${compResult}!'; 
+        
+        const winLose = play(playerChoice,compResult);
+        choiceText.textContent = 'You choose: '+ playerChoice;
+        resultText.textContent = 'You get: '+compResult; 
+        winLoseText.textContent = 'You: ' +winLose;
+        if(compResult == "heads"){
+            image = "heads.png";
+        }else{
+            image = "tails.png";
+        }
+        document.getElementById('changeCat').src = image;
     });
 });
